92810514277742ad7cb5b841ca859243de52dd1b
Pavel Tsveroshvili <mahanpt13@gmail.com>
2022-08-17 20:21:06 +0300

    1

--- backend/app.js
+++ backend/app.js
@@ -13,21 +13,49 @@ const routUsers = require('./routes/users');
 const routcards = require('./routes/cards');
 
 // Слушаем порт
-const { PORT = 3001 } = process.env;
-const options = {
-  origin: [
-    'http://localhost:3000',
-    'https://github.com/lemonlemongit',
-    'http://domainname.lemon.nomoredomains.sbs/',
-    'http://api.domainname.lemon.nomoredomains.sbs/',
-    'https://domainname.lemon.nomoredomains.sbs/',
-    'https://api.domainname.lemon.nomoredomains.sbs/',
-  ],
-  credentials: true,
-};
+const { PORT = 3000 } = process.env;
+//const options = {
+  //origin: [
+   // 'http://localhost:3000',
+
+    //'http://domainname.lemon.nomoredomains.sbs/',
+    //'http://api.domainname.lemon.nomoredomains.sbs/',
+   // 'https://domainname.lemon.nomoredomains.sbs/',
+    //'https://api.domainname.lemon.nomoredomains.sbs/',
+  //],
+ // credentials: true,
+//;
+
+// Массив доменов, с которых разрешены кросс-доменные запросы
+const allowedCors = [
+  'http://domainname.lemon.nomoredomains.sbs/',
+  'http://api.domainname.lemon.nomoredomains.sbs/',
+  'https://domainname.lemon.nomoredomains.sbs',
+  'https://api.domainname.lemon.nomoredomains.sbs',
+  'localhost:3000'
+];
+app.use(function(req, res, next) {
+  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
+  const requestHeaders = req.headers['access-control-request-headers']; // сохраняем список заголовков исходного запроса
+  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS";
+  if (allowedCors.includes(origin)) {
+    // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
+    res.header('Access-Control-Allow-Origin', origin);
+}
+  if (method === 'OPTIONS') {
+    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+}
+  next();
+});
 
 const app = express();
-app.all('*', cors(options));
+
+app.all('*', cors(allowedCors));
+//app.all('*', cors(options));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
