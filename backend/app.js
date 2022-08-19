684854d5d03350a406cc9445e0faccd6ed3b2e71
lemonlemongit <94648774+lemonlemongit@users.noreply.github.com>
2022-08-19 12:07:33 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors, celebrate, Joi } = require('celebrate');
-// const cors = require('cors');
+ const cors = require('cors');
 const { regexp } = require('./regexp/regexp');
 const { login, createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
@@ -60,7 +60,7 @@ app.use((req, res, next) => {
 });
 
 // app.all('*', cors(allowedCors));
-// app.all('*', cors(options));
+ app.all('*', cors(options));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
