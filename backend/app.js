68fb7f5f222c5c4cc30c40d5abc3a930d5465697
lemonlemongit <94648774+lemonlemongit@users.noreply.github.com>
2022-08-21 20:51:55 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -60,7 +60,7 @@ app.use((req, res, next) => {
   next();
 });
 
- app.all('*', cors(allowedCors));
+ // app.all('*', cors(allowedCors));
 // app.all('*', cors(options));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
