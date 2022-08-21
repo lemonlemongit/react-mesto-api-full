c36ac3e2c553b16d45e1c89ddf34207596f343a0
lemonlemongit <94648774+lemonlemongit@users.noreply.github.com>
2022-08-21 18:11:40 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -43,7 +43,7 @@ app.use((req, res, next) => {
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   if (allowedCors.includes(origin)) {
     // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
-    // res.header('Access-Control-Allow-Origin', allowedCors);
+    res.header('Access-Control-Allow-Origin', allowedCors);
     res.header('Access-Control-Allow-Origin', origin);
    // res.header('Access-Control-Request-Header', 'https://domainname.lemon.nomoredomains.sbs');
     res.header('Access-Control-Allow-Credentials', true);
@@ -54,7 +54,7 @@ app.use((req, res, next) => {
    // res.header('Access-Control-Allow-Headers', requestHeaders, origin, X-Requested-With, Content-Type, Accept );
    // res.header('Access-Control-Allow-Credentials', true);
    res.header('Access-Control-Allow-Origin', allowedCors);
-   res.header('Access-Control-Allow-Headers', requestHeaders)
+   // res.header('Access-Control-Allow-Headers', requestHeaders)
    return res.end();
   }
   next();
