b2861fb5cbedfeacfec35a7e220fa8cead9f9225
lemonlemongit <94648774+lemonlemongit@users.noreply.github.com>
2022-08-21 20:41:25 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -12,56 +12,56 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const routUsers = require('./routes/users');
 const routcards = require('./routes/cards');
 
+// Слушаем порт
+const { PORT = 3000 } = process.env;
+ // const options = {
+// origin: [
+// 'http://localhost:3000',
 
-  const options = {
- origin: [
- 'http://localhost:3000',
- 'http://domainname.lemon.nomoredomains.sbs',
- 'http://api.domainname.lemon.nomoredomains.sbs',
- 'https://domainname.lemon.nomoredomains.sbs',
- 'https://api.domainname.lemon.nomoredomains.sbs',
- ],
- credentials: true,
- ;
+// 'http://domainname.lemon.nomoredomains.sbs/',
+// 'http://api.domainname.lemon.nomoredomains.sbs/',
+// 'https://domainname.lemon.nomoredomains.sbs/',
+// 'https://api.domainname.lemon.nomoredomains.sbs/',
+// ],
+// credentials: true,
+// ;
 
 // Массив доменов, с которых разрешены кросс-доменные запросы
-// const allowedCors = [
-//   'http://domainname.lemon.nomoredomains.sbs/',
-//  'http://api.domainname.lemon.nomoredomains.sbs/',
-//  'https://domainname.lemon.nomoredomains.sbs',
-// 'https://api.domainname.lemon.nomoredomains.sbs',
-//  'localhost:3000',
-//];
-// Слушаем порт
-const { PORT = 3001 } = process.env;    
+const allowedCors = [
+  'http://domainname.lemon.nomoredomains.sbs/',
+  'http://api.domainname.lemon.nomoredomains.sbs/',
+  'https://domainname.lemon.nomoredomains.sbs',
+ 'https://api.domainname.lemon.nomoredomains.sbs',
+  'localhost:3000',
+];
 const app = express();
 
-// app.use((req, res, next) => {
- // const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
- // const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
- // const requestHeaders = req.headers['access-control-request-headers']; // сохраняем список заголовков исходного запроса
-//  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-//  if (allowedCors.includes(origin)) {
+app.use((req, res, next) => {
+  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
+  const requestHeaders = req.headers['access-control-request-headers']; // сохраняем список заголовков исходного запроса
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  if (allowedCors.includes(origin)) {
     // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
    // res.header('Access-Control-Allow-Origin', allowedCors);
- //   res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Origin', origin);
    // res.header('Access-Control-Request-Header', 'https://domainname.lemon.nomoredomains.sbs');
- //   res.header('Access-Control-Allow-Credentials', true);
- // }
- // if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Credentials', true);
+  }
+  if (method === 'OPTIONS') {
     // разрешаем кросс-доменные запросы любых типов (по умолчанию)
-  //  res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
    // res.header('Access-Control-Allow-Headers', requestHeaders, origin, X-Requested-With, Content-Type, Accept );
    // res.header('Access-Control-Allow-Credentials', true);
   // res.header('Access-Control-Allow-Origin', allowedCors);
- //  res.header('Access-Control-Allow-Headers', requestHeaders)
-//   return res.end();
- // }
-//  next();
-// });
+   res.header('Access-Control-Allow-Headers', requestHeaders)
+   return res.end();
+  }
+  next();
+});
 
 // app.all('*', cors(allowedCors));
- app.all('*', cors(options));
+// app.all('*', cors(options));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
